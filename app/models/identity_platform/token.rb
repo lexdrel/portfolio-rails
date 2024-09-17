16741d6ef13f4733ccb4616c6dd453b15d2f2f34
Alex Castillo <alex@lexdrel.com>
2024-09-17 17:56:01 +0000

    initial commit

--- app/models/identity_platform/token.rb
+++ app/models/identity_platform/token.rb
@@ -50,7 +50,7 @@ module IdentityPlatform
     end
 
     def self.expected_audience
-      ENV.fetch 'GOOGLE_CLOUD_PROJECT', 'rails-google-cloud-quickstart'
+      ENV.fetch 'GOOGLE_CLOUD_PROJECT', 'portfolio-rails'
     end
 
     def self.expected_issuer
